9c201e9eef4fce10e3b64c4fc3c2e85e23717e7a
mkujun <kujundzic.marko@hotmail.com>
2026-01-08 13:12:56 +0100

    day 2, part 2

--- 2025/Day2/src/Main.java
+++ 2025/Day2/src/Main.java
@@ -1,8 +1,10 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class Main {
+    static Set<String> unique = new HashSet<>();
+
     public static void main(String[] args) {
         //Path filePath = Paths.get("Day2/src/example.txt");
         Path filePath = Paths.get("Day2/src/input.txt");
@@ -16,18 +18,47 @@ public class Main {
             long from = Long.parseLong(id.split("-")[0]);
             long to = Long.parseLong(id.split("-")[1]);
 
-            for (long i = from; i < to; i++) {
-                int mid = String.valueOf(i).length() / 2;
+            for (long i = from; i <= to; i++) {
 
-                String firstHalf = String.valueOf(i).substring(0, mid);
-                String secondHalf = String.valueOf(i).substring(mid);
+                int numInRangeLength = String.valueOf(i).length();
 
-                if (firstHalf.equals(secondHalf)) {
-                    sum = sum + i;
+                for (long j = 2; j <= numInRangeLength; j++) {
+                    if (numInRangeLength % j == 0) {
+                        splitIntoParts(j,i);
+                    }
                 }
             }
         }
 
+        for (String value : unique) {
+            sum = sum + Long.valueOf(value);
+        }
         System.out.println(sum);
+    }
+
+    private static void splitIntoParts(long j, long numberToSplit) {
+        int partSize = (int) (String.valueOf(numberToSplit).length() / j);
+        String[] result = new String[(int)j];
+
+        for (int i = 0; i < j; i++) {
+            int start = i * partSize;
+            result[i] = String.valueOf(numberToSplit).substring(start, start + partSize);
+        }
+
+        if (arePartsEqual(result)) {
+            String concatParts = String.join("", result);
+            unique.add(concatParts);
+        }
+
+
+    }
+
+    private static boolean arePartsEqual(String[] parts) {
+        for (int i = 1; i < parts.length; i++) {
+            if (!Objects.equals(parts[i], parts[i - 1])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
