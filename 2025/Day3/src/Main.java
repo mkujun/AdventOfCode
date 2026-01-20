1f30e897b8d651024264b4ef2d70d0692e748e35
mkujun <kujundzic.marko@hotmail.com>
2026-01-20 09:19:35 +0100

    day 3, part 2

--- 2025/Day3/src/Main.java
+++ 2025/Day3/src/Main.java
@@ -1,5 +1,6 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -7,38 +8,42 @@ public class Main {
         //Path filePath = Paths.get("Day3/src/example.txt");
         Path filePath = Paths.get("Day3/src/input.txt");
         List<String> banks = Utils.readFile(filePath);
-        int totalJoltage = 0;
+        long totalJoltage = 0;
 
         for (String bank: banks) {
             int[] digits = bank.chars()
                     .map(c -> c - '0')
                     .toArray();
 
-            int bat1 = digits[0];
-            int bat1Index = 0;
-            for (int i = 1; i < digits.length - 1 ; i++) {
-                if (digits[i] > bat1) {
-                    bat1 = digits[i];
-                    bat1Index = i;
-                }
-            }
-            int bat2 = digits[bat1Index + 1];
-            for (int i = bat1Index + 1; i < digits.length; i++) {
-                if (digits[i] > bat2) {
-                    bat2 = digits[i];
-                }
-            }
-            /*
-            System.out.println("bat1 " + bat1);
-            System.out.println("bat2 " + bat2);
-            System.out.println();
+            List<String> joltage = new ArrayList<>();
 
-             */
+            int beginIndex = 0;
 
-            String joltage = String.valueOf(bat1) + String.valueOf(bat2);
-            totalJoltage = totalJoltage + Integer.parseInt(joltage);
+            for (int i = 11; i >= 0; i--) {
+                int endIndex = digits.length - i;
+                int maxIndex = findMaxIndex(beginIndex, endIndex, digits);
+
+                joltage.add(String.valueOf(digits[maxIndex]));
+
+                beginIndex = maxIndex + 1;
+            }
+
+            totalJoltage = totalJoltage + Long.parseLong(String.join("", joltage));
         }
 
         System.out.println(totalJoltage);
+    }
+
+    private static int findMaxIndex(int beginIndex, int endIndex, int[] digits) {
+        int maxValue = digits[beginIndex];
+        int maxIndex = beginIndex;
+        for (int i = beginIndex; i < endIndex; i++) {
+            if (digits[i] > maxValue) {
+                maxValue = digits[i];
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
     }
 }
